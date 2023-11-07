91ef6a24e23db6e89c0727e67e318e3200d29031
korayagithub <148747492+korayagithub@users.noreply.github.com>
2023-11-07 21:41:51 +0800

    Create SumOfIntegerInputs.java

---
+++ SumOfIntegerInputs.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        if(args.length == 0)
+        {
+            System.out.println("Please input numbers in the arguments!");
+        }
+
+        int sum = 0;
+        for (int num = 0; num < args.length; num++)
+        {
+            sum += Integer.parseInt (args[num]);
+        }
+        System.out.println("The sum of all the numbers (in the arguments) is " + sum + ".");
+    }
+}
